e486b46d2ba1b3b72f70d7f556382087c09736d9
Hippo <badrihippo@gmail.com>
2016-03-15 14:14:22 +0530

    Add user import function

--- merlout.py
+++ merlout.py
@@ -25,11 +25,33 @@ def write_usergroups(reader):
     for d in reader:
         models.UserGroup.objects(name=d[1]).update(set__position=d[0], upsert=True)
 
+def write_users(reader):
+    '''
+    Import Users from a csv DictReader
+    '''
+
+    for d in reader:
+        try:
+            group = models.UserGroup.objects.get(name=d['GroupName'].strip())
+        except models.db.DoesNotExist:
+            group = models.UserGroup.objects.create(name=d['GroupName'].strip())
+
+        email = d['Email'].strip() if d['Email'] else None
+        name = d['UserName'].strip()
+        username = name.lower()
+        
+        models.User.objects(name=name).update(set__group=group, set__email=email, set__username=username, upsert=True)
+
 def process_all():
     print 'Importing UserGroups...'
     with open('List_of_Groups.csv', 'rb') as f:
         r = csv.reader(f)
         write_usergroups(r)
+
+    print 'Import Users...'
+    with open('List_of_Users.csv', 'rb') as f:
+        r = csv.DictReader(f)
+        write_users(r)
 
 if __name__ == '__main__':
     process_all()
