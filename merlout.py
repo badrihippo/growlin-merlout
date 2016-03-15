a967eb5cc6657a57ebf772db0dd608809d2ede32
Hippo <badrihippo@gmail.com>
2016-03-15 15:59:46 +0530

    Add basic metadata importers
    Namely: Currency, Publisher, PublishePlace, CampusLocation

--- merlout.py
+++ merlout.py
@@ -42,16 +42,72 @@ def write_users(reader):
         
         models.User.objects(name=name).update(set__group=group, set__email=email, set__username=username, upsert=True)
 
+def write_currencies(reader):
+    '''
+    Import Currencies from a csv DictReader
+    '''
+    for d in reader:
+        currency = d['Currency'].strip()
+        models.Currency.objects(name=currency).update(set__symbol=currency[:4], upsert=True)
+
+def write_publishers(reader):
+    '''
+    Import Publisher from a csv DictReader
+    '''
+    for d in reader:
+        name = d['Publisher'].strip()
+        models.Publisher.objects(name=name).update(set__name=name, upsert=True)
+
+def write_publishplaces(reader):
+    '''
+    Import PublishPlaces from a csv DictReader
+    '''
+    for d in reader:
+        name = d['Place of Publication'].strip()
+        models.PublishPlace.objects(name=name).update(set__name=name, upsert=True)
+
+def write_locations(reader):
+    '''
+    Import CampusLocations from a csv DictReader
+    '''
+    for d in reader:
+        name = d['Location'].strip()
+        try:
+            prevent_borrowing = bool(int(d['PreventBorrow'].strip()))
+        except ValueError:
+            prevent_borrowing = False
+        models.CampusLocation.objects(name=name).update(set__prevent_borrowing=prevent_borrowing, upsert=True)
+
 def process_all():
     print 'Importing UserGroups...'
     with open('List_of_Groups.csv', 'rb') as f:
         r = csv.reader(f)
         write_usergroups(r)
 
-    print 'Import Users...'
+    print 'Importing Users...'
     with open('List_of_Users.csv', 'rb') as f:
         r = csv.DictReader(f)
         write_users(r)
+
+    print 'Importing Currencies...'
+    with open('List_of_Currencies.csv', 'rb') as f:
+        r = csv.DictReader(f)
+        write_currencies(r)
+
+    print 'Importing Publishers...'
+    with open('List_of_Publishers.csv', 'rb') as f:
+        r = csv.DictReader(f)
+        write_publishers(r)
+
+    print 'Importing PublishPlaces...'
+    with open('List_of_Places_of_Publication.csv', 'rb') as f:
+        r = csv.DictReader(f)
+        write_publishplaces(r)
+
+    print 'Importing CampusLocations...'
+    with open('List_of_Locations.csv', 'rb') as f:
+        r = csv.DictReader(f)
+        write_locations(r)
 
 if __name__ == '__main__':
     process_all()
