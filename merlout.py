6fae1b10d9b15bd8537e6c610f5b9edf2aa53957
Hippo <badrihippo@gmail.com>
2016-03-15 13:48:18 +0530

    Add (basic) import UserGroup function

--- merlout.py
+++ merlout.py
@@ -11,6 +11,26 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),'../gro
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),'../growlin/flask-admin-material'))
 
 import csv
-from growlin.models import *
+from growlin import models
 
-# TODO: Actually do stuff
+def write_usergroups(reader):
+    '''
+    Import User Groups from a csv reader serving a file of format:
+    ['GroupID', 'GroupName']
+    '''
+
+    if reader.next() != ['GroupID', 'GroupName']:
+        raise ValueError("CSV format must be ['GroupID', 'GroupName']")
+
+    for d in reader:
+        models.UserGroup.objects.create(position=d[0], name=d[1])
+
+def process_all():
+    print 'Importing UserGroups...'
+    with open('List_of_Groups.csv', 'rb') as f:
+        r = csv.reader(f)
+        write_usergroups(r)
+
+if __name__ == '__main__':
+    process_all()
+
